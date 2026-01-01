ec8d1ca0736df198d7852e0603a2ff22276dcb8a
rly09 <yogiroshan2005@gmail.com>
2026-01-01 21:03:45 +0530

    solving errors

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@ import 'package:kpg_shop/main.dart';
 void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const ProviderScope(child: KGSApp()));
+    await tester.pumpWidget(const ProviderScope(child: KGSShopApp()));
 
     // Verify that role selection screen is shown
     expect(find.text('KGS'), findsOneWidget);
